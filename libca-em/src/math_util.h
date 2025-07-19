38d5ed90717740887a67d152ecd85406902bed3e
Canrad <1517807724@qq.com>
2025-07-19 23:13:39 +0800

    新建一个libca-em库

---
+++ libca-em/src/math_util.h
@@ -0,0 +1,15 @@
+#ifndef MATH_UTIL_H
+#define MATH_UTIL_H
+
+typedef struct {
+  float x;
+  float y;
+}vec2f;
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+}vec3f_t;
+
+#endif // !MATH_UTIL_H
