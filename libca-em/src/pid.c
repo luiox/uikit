a2ee6b04648ad1121e31a389ebcbdd1a533160ac
Canrad <1517807724@qq.com>
2025-07-21 19:32:08 +0800

    添加pid实现

---
+++ libca-em/src/pid.c
@@ -0,0 +1,80 @@
+/**
+ * @file pid.c
+ * @author canrad (1517807724@qq.com)
+ * @brief pid算法，包含位置式和增量式pid计算
+ * @version 0.1
+ * @date 2024-08-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "pid.h"
+
+void pid_position_init(pid_position_t* pid, float kp, float ki, float kd, float target)
+{
+    pid->target     = target;
+    pid->kp         = kp;
+    pid->ki         = ki;
+    pid->kd         = kd;
+    pid->last_error = 0.0;
+    pid->prev_error = 0.0;
+    pid->sum_error  = 0.0;
+}
+
+float pid_position_calculate(pid_position_t* pid, float current_value)
+{
+    // 离散的位置式PID计算公式
+    // u(k)=Kp*e(k)+Ki*∑e(j)+Kd[e(k)-e(k-1)]
+    // 其中：
+    // u(k)为第k次采样时刻的PID控制输出
+    // e(k)为第k次采样时刻输入的偏差值，e(k)=r(k)-y(k)
+    // r(k)为第k次采样时刻的设定值
+    // y(k)为第k次采样时刻的反馈值
+
+    float error = pid->target - current_value;   // 当前误差 = 目标值 - 当前值
+    pid->sum_error += error;                     // 误差积分
+    float d_error = error - pid->last_error;     // 误差微分
+
+    pid->prev_error = pid->last_error;   // 更新上一次的误差
+    pid->last_error = error;             // 更新当前最新的误差
+
+    return pid->kp * error              // 比例项
+           + pid->ki * pid->sum_error   // 积分项
+           + pid->kd * d_error;         // 微分项
+}
+
+void pid_incremental_init(pid_incremental_t* pid, float kp, float ki, float kd, float target)
+{
+    pid->target     = target;
+    pid->kp         = kp;
+    pid->ki         = ki;
+    pid->kd         = kd;
+    pid->last_error = 0.0;
+    pid->prev_error = 0.0;
+}
+
+float pid_incremental_calculate(pid_incremental_t* pid, float current_value)
+{
+    // 离散的增量式PID计算公式
+    // 从位置式PID的公式中得到u(k)，带入k-1，得到u(k-1)
+    // 得到△u(k)=u(k)-u(k-1)
+    // △u(k)=Ae(k)-Be(k-1)+Ce(k-2)
+    // 其中：
+    // A=Kp+Ki+Kd
+    // B=Kp+2Kd
+    // C=Kd
+    // 加上u(k)=u(k-1)+△u(k)，再展开，得到增量式PID公式如下
+    // u(k)=u(k-1)+Kp[e(k)-e(k-1)]+Ki*e(k)+Kd[e(k)-2e(k-1)+e(k-2)]
+
+    // 注意pid_incremental_calculate这个函数计算的是△u(k)，也就是增量，而不是u(k)
+
+    float error  = pid->target - current_value;   // 当前误差 = 目标值 - 当前值
+    float output = pid->kp * error                // E[k]
+                   - pid->ki * pid->last_error    // - E[k-1]
+                   + pid->kd * pid->prev_error;   // + E[k-2]
+    pid->prev_error = pid->last_error;
+    pid->last_error = error;
+
+    return output;
+}
---
+++ libca-em/src/pid.h
@@ -0,0 +1,132 @@
+/**
+ * @file pid.h
+ * @author canrad (1517807724@qq.com)
+ * @brief pid算法，包含位置式和增量式pid计算
+ * 使用方法：
+ * 1. 创建pid对象，并初始化
+ *   pid_position_t pid;
+ *   pid_position_init(&pid, 0.1, 0.01, 0.001, 100);
+ * 2. pid计算，并应用于系统（这里以电机设置占空比为例）
+ *   int duty = pid_position_calculate(&pid, current_value);
+ *   motor_set_duty(duty);
+ *
+ * @version 0.1
+ * @date 2024-08-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef MYLIB_UTILITY_PID_H
+#define MYLIB_UTILITY_PID_H
+
+/**
+ * @brief 位置式pid对象
+ * 
+ */
+typedef struct
+{
+    float target;       // 设定目标值 
+    float kp;           // 比例系数 
+    float ki;           // 积分系数 
+    float kd;           // 微分系数 
+    float last_error;   // 最后一次的误差 
+    float prev_error;   // 上一次误差 
+    float sum_error;    // 误差和 
+} pid_position_t;
+
+/**
+ * @brief 位置式pid结构体初始化
+ * 
+ * @param pid pid对象
+ * @param kp 比例系数
+ * @param ki 积分系数
+ * @param kd 微分系数
+ * @param target 目标值
+ */
+void pid_position_init(pid_position_t* pid, float kp, float ki, float kd, float target);
+
+/**
+ * @brief 位置式pid设置目标值
+ * 
+ * @param pid pid对象
+ * @param target 目标值
+ */
+inline static void pid_position_set_target(pid_position_t* pid, float target)
+{
+    pid->target = target;
+}
+
+/**
+ * @brief 位置式pid计算
+ * 
+ * @param pid pid对象
+ * @param current_value 当前pid输入值
+ * @return 位置式pid计算后的u(k) 
+ */
+float pid_position_calculate(pid_position_t* pid, float current_value);
+
+/**
+ * @brief 增量式pid对象
+ * 
+ */
+typedef struct
+{
+    float target;       // 设定目标值
+    float kp;           // 比例系数 
+    float ki;           // 积分系数 ki=kp*T/Ti
+    float kd;           // 微分系数 kd=kp*Td/T
+    float last_error;   // 最后一次的误差 
+    float prev_error;   // 上一次误差
+} pid_incremental_t;
+ 
+/**
+ * @brief 增量式pid结构体初始化
+ * 
+ * @param pid pid对象
+ * @param kp 比例系数
+ * @param ki 积分系数
+ * @param kd 微分系数
+ * @param target 目标值
+ */
+void pid_incremental_init(pid_incremental_t* pid, float kp, float ki, float kd, float target);
+
+/**
+ * @brief 增量式pid设置目标值
+ * 
+ * @param pid pid对象
+ * @param target 目标值
+ */
+inline static void pid_incremental_set_target(pid_incremental_t* pid, float target)
+{
+    pid->target = target;
+}
+
+/**
+ * @brief 增量式pid计算
+ * 
+ * @param pid pid对象
+ * @param current_value 当前pid输入值
+ * @return 增量式pid计算后的△u(k) 
+ */
+float pid_incremental_calculate(pid_incremental_t* pid, float current_value);
+
+/**
+ * @brief 限幅函数，将输入值限制在min和max之间
+ * 
+ * @param value 输入值
+ * @param min 最大值
+ * @param max 最小值
+ * @return 限幅后的值 
+ */
+static inline float clamp(float value, float min, float max)
+{
+    if (value < min) {
+        return min;
+    } else if (value > max) {
+        return max;
+    }
+    return value;
+}
+
+#endif   // !MYLIB_UTILITY_PID_H
