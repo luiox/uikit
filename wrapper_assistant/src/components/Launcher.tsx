96818a172622955483351c9c60b8b5f1a425ab90
Canrad <1517807724@qq.com>
2025-08-19 16:31:51 +0800

    修改顶部标题栏

--- wrapper_assistant/src/components/Launcher.tsx
+++ wrapper_assistant/src/components/Launcher.tsx
@@ -20,42 +20,47 @@ const Launcher: React.FC = () => {
 
   return (
     <div className="launcher">
-      {/* 左侧分类菜单 */}
-      <div className="menu">
-        <Title level={4} style={{ margin: '16px 0 8px 16px' }}>Poner</Title>
-        <Menu onClick={handleMenuClick} selectedKeys={[selectedCategory]} mode="inline">
-          {categories.map((category) => (
-            <Menu.Item key={category}>{category}</Menu.Item>
-          ))}
-        </Menu>
+      {/* 顶部标题栏 */}
+      <div className="launcher-header">
+        <Title level={3} style={{ margin: 0, padding: '12px 24px' }}>Poner</Title>
       </div>
-      {/* 右侧启动项列表 */}
-      <div className="list">
-        <List
-          header={<div style={{ fontWeight: 600, fontSize: 16 }}>{selectedCategory}</div>}
-          itemLayout="horizontal"
-          dataSource={getItemsByCategory(selectedCategory)}
-          renderItem={(item: LaunchItem) => {
-            // 只允许 http(s) 或 data:image 开头的路径作为 src，否则用默认头像
-            let avatarSrc: string | undefined = undefined;
-            if (item.IconLocation &&
-              (item.IconLocation.startsWith('http://') ||
-               item.IconLocation.startsWith('https://') ||
-               item.IconLocation.startsWith('data:image'))
-            ) {
-              avatarSrc = item.IconLocation;
-            }
-            return (
-              <List.Item>
-                <List.Item.Meta
-                  avatar={<Avatar src={avatarSrc} >{!avatarSrc && item.Name[0]}</Avatar>}
-                  title={item.Name}
-                  description={item.TargetPath}
-                />
-              </List.Item>
-            );
-          }}
-        />
+      <div className="launcher-body">
+        {/* 左侧分类菜单 */}
+        <div className="menu">
+          <Menu onClick={handleMenuClick} selectedKeys={[selectedCategory]} mode="inline">
+            {categories.map((category) => (
+              <Menu.Item key={category}>{category}</Menu.Item>
+            ))}
+          </Menu>
+        </div>
+        {/* 右侧启动项列表 */}
+        <div className="list">
+          <List
+            header={<div style={{ fontWeight: 600, fontSize: 16 }}>{selectedCategory}</div>}
+            itemLayout="horizontal"
+            dataSource={getItemsByCategory(selectedCategory)}
+            renderItem={(item: LaunchItem) => {
+              // 只允许 http(s) 或 data:image 开头的路径作为 src，否则用默认头像
+              let avatarSrc: string | undefined = undefined;
+              if (item.IconLocation &&
+                (item.IconLocation.startsWith('http://') ||
+                 item.IconLocation.startsWith('https://') ||
+                 item.IconLocation.startsWith('data:image'))
+              ) {
+                avatarSrc = item.IconLocation;
+              }
+              return (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={<Avatar src={avatarSrc} >{!avatarSrc && item.Name[0]}</Avatar>}
+                    title={item.Name}
+                    description={item.TargetPath}
+                  />
+                </List.Item>
+              );
+            }}
+          />
+        </div>
       </div>
     </div>
   );
