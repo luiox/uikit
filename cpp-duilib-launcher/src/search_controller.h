510a571c3d15da6b50de2dcd46542fe15f8b14e5
Canrad <1517807724@qq.com>
2026-07-27 21:56:34 +0800

    feat(search): implement command parsing in SearchController
    - ParseCommand(): match cmd/setting/shutdown/reboot/logoff/screenoff/baidu <keyword>
    - HandleInputChanged(): parse command before triggering render
    - CommandIdToItemId(): map command ID to list item ID with __cmd__ prefix

--- cpp-duilib-launcher/src/search_controller.h
+++ cpp-duilib-launcher/src/search_controller.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+
+#include "constants.h"
+
 class AppWindow;
 
 class SearchController {
@@ -11,6 +15,14 @@ public:
     void ToggleSearchMode();
     void HandleInputChanged();
 
+    int GetActiveCommand() const { return active_command_; }
+    const std::string& GetBaiduKeyword() const { return baidu_keyword_; }
+
+    static int ParseCommand(const std::string& input, std::string* out_keyword);
+    static std::string CommandIdToItemId(int cmd_id);
+
 private:
     AppWindow& owner_;
+    int active_command_ = launcher::constants::search_cmd::kNone;
+    std::string baidu_keyword_;
 };
