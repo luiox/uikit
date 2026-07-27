cfe919446fd2d36d66a3b0f1a0acdcbeeba15063
Canrad <1517807724@qq.com>
2026-07-27 21:56:46 +0800

    feat(search): render command entries in search mode
    When a search command is active, render a styled command entry
    with blue title and gray description instead of normal app items.

--- cpp-duilib-launcher/src/list_controller.cpp
+++ cpp-duilib-launcher/src/list_controller.cpp
@@ -4,6 +4,7 @@
 
 #include "app_window.h"
 #include "file_icon_control.h"
+#include "search_controller.h"
 #include "utils/string_util.h"
 
 using namespace DuiLib;
@@ -58,6 +59,73 @@ void ListController::RenderItems() {
         std::string keyword;
         if (owner_.search_input_ != nullptr) {
             keyword = launcher::util::WideToUtf8(owner_.search_input_->GetText().GetData());
+        }
+
+        const int active_cmd = owner_.search_controller_.GetActiveCommand();
+
+        if (active_cmd != launcher::constants::search_cmd::kNone) {
+            auto add_command_row = [&](const std::wstring& label, const std::wstring& desc, int cmd_id) {
+                auto* row = new CListContainerElementUI();
+                row->SetFixedHeight(34);
+                row->SetAttribute(_T("inset"), _T("4,0,4,0"));
+                row->SetAttribute(_T("childpadding"), _T("6"));
+                row->SetAttribute(_T("childvalign"), _T("vcenter"));
+
+                auto* icon = new FileIconControl();
+                icon->SetFixedWidth(26);
+                icon->SetFixedHeight(26);
+                icon->SetBkColor(0xFFE8F0FE);
+                row->Add(icon);
+
+                auto* text_layout = new CVerticalLayoutUI();
+                text_layout->SetAttribute(_T("childpadding"), _T("0"));
+
+                auto* name = new CLabelUI();
+                name->SetText(label.c_str());
+                name->SetTextColor(0xFF1A73E8);
+                name->SetTextStyle(DT_LEFT | DT_VCENTER | DT_SINGLELINE | DT_END_ELLIPSIS);
+                text_layout->Add(name);
+
+                if (!desc.empty()) {
+                    auto* desc_label = new CLabelUI();
+                    desc_label->SetText(desc.c_str());
+                    desc_label->SetTextColor(0xFF808689);
+                    desc_label->SetTextStyle(DT_LEFT | DT_VCENTER | DT_SINGLELINE | DT_END_ELLIPSIS);
+                    desc_label->SetAttribute(_T("font"), _T("1"));
+                    text_layout->Add(desc_label);
+                }
+
+                row->Add(text_layout);
+
+                owner_.items_list_->Add(row);
+                owner_.item_ids_.push_back(SearchController::CommandIdToItemId(cmd_id));
+                owner_.item_group_ids_.push_back("");
+            };
+
+            switch (active_cmd) {
+            case launcher::constants::search_cmd::kCmd:
+                add_command_row(L"Open Command Prompt", L"cmd", active_cmd);
+                break;
+            case launcher::constants::search_cmd::kSettings:
+                add_command_row(L"Open System Settings", L"setting", active_cmd);
+                break;
+            case launcher::constants::search_cmd::kShutdown:
+                add_command_row(L"Shut Down Computer", L"shutdown", active_cmd);
+                break;
+            case launcher::constants::search_cmd::kReboot:
+                add_command_row(L"Restart Computer", L"reboot", active_cmd);
+                break;
+            case launcher::constants::search_cmd::kLogoff:
+                add_command_row(L"Log Off Current User", L"logoff", active_cmd);
+                break;
+            case launcher::constants::search_cmd::kScreenoff:
+                add_command_row(L"Turn Off Display", L"screenoff", active_cmd);
+                break;
+            case launcher::constants::search_cmd::kBaidu:
+                add_command_row(L"Search Baidu", launcher::util::Utf8ToWide(owner_.search_controller_.GetBaiduKeyword()), active_cmd);
+                break;
+            }
+            return;
         }
 
         for (const auto& group : owner_.backend_.Data().groups) {
